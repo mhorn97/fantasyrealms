e46c34e14579e2ed7397afd01a6450f7277c0052
Michael Horn <michaelhorn1@hotmail.com>
2018-03-17 19:26:45 -0400

    small changes

--- tmp/1yxmmt19facgg.1dgu6fxm26921.php
+++ tmp/1yxmmt19facgg.1dgu6fxm26921.php
@@ -18,7 +18,12 @@
 <div class="container" id="maindiv">
     <div class="row">
         <div class="col-sm-6" id="col1">
-            <h2>Welcome Back <?= ($username) ?></h2>
+            <div class="container" id="introduction">
+                <h2>Welcome Back <?= ($username) ?></h2>
+                <br><br>
+                <p>Welcome back to Fantasy Realms! A world for the most adventurous and brave. It has magic, monsters, and mysterious, unexplored lands. Take up arms and
+                control your journey</p>
+            </div>
         </div>
         <div class="col-sm-6" id="col2">
             <div class="container" id="character-select">
@@ -26,13 +31,12 @@
 
 
                     <?php foreach (($characters?:[]) as $character): ?>
-                        <ol>
-                            <li><p><?= ($character['name']) ?>, <?= ($character['race']) ?>, <?= ($character['class']) ?></p>
-                                <a href="<?= ($BASE) ?>/summary/<?= ($character['characterId']) ?>"><button name="view">View Character</button></a>
-                                <a href="<?= ($BASE) ?>/story-part1/<?= ($character['characterId']) ?>"><button name = "resume">Story</button></a>
-                                <button name = "delete" id="delete<?= ($character['characterId']) ?>" value="<?= ($character['characterId']) ?>">Delete</button>
-                            </li>
-                    </ol>
+                        <div class="container" id="characterContainer">
+                            <p>|Name: <?= ($character['name']) ?> | Race: <?= ($character['race']) ?> | Class: <?= ($character['class']) ?>|</p>
+                            <a href="<?= ($BASE) ?>/summary/<?= ($character['characterId']) ?>"><button name="view">View Character</button></a>
+                            <a href="<?= ($BASE) ?>/story-part1/<?= ($character['characterId']) ?>"><button name = "resume">Story</button></a>
+                            <button name = "delete" id="delete<?= ($character['characterId']) ?>" value="<?= ($character['characterId']) ?>">Delete</button>
+                        </div>
                     <?php endforeach; ?>
             <br>
             <br>
